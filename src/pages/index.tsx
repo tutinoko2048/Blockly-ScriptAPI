a8611667ca718453ac771199629a36812595ef68
Retoruto9900K <tutinoko2048@gmail.com>
2023-07-21 07:04:41 +0000

    app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,40 @@ import Blockly from 'blockly';
 import { useState, useEffect } from 'react';
 
 export default function App() {
+  const xml = `<xml id='toolbox'>
+    <category name="Loops" colour="%{BKY_LOOPS_HUE}">
+        <block type="controls_repeat_ext">
+            <value name="TIMES">
+                <block type="math_number">
+                    <field name="NUM">10</field>
+                </block>
+            </value>
+        </block>
+        <block type="controls_whileUntil"></block>
+    </category>
+    <category name="Math" colour="%{BKY_MATH_HUE}">
+        <block type="math_number">
+            <field name="NUM">123</field>
+        </block>
+    </category>
+    <category name="Text" colour="%{BKY_TEXTS_HUE}">
+        <block type="text"></block>
+        <block type="text_print"></block>
+    </category>
+    </xml>`
+  const parser = new DOMParser();
+  const dom = parser.parseFromString(xml, 'text/xml');
+  
   useEffect(() => {
     Blockly.inject('workspace', {
-      toolbox: document.getElementById('toolbox')
+      toolbox: dom.getElementById('toolbox') as Element
     });
   });
   
   return (
     <main>
       <div id="workspace" style={{ width: '300px', height: '300px' }}></div>
-      <div id='toolbox' > </div>
+      
       hi
     </main>
   )
